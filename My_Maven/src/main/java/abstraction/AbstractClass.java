b7ae8dc25af12d348d9d60689f4dbf0c56eb47e6
hp <hp@LAPTOP-TSBJIJT7>
2024-09-23 08:04:44 +0530

    second commit

--- My_Maven/src/main/java/abstraction/AbstractClass.java
+++ My_Maven/src/main/java/abstraction/AbstractClass.java
@@ -4,5 +4,6 @@ public abstract class AbstractClass {//abstract class with abstract keyword
   public abstract void print(); // abstract method
   public void display() {// non abstract method
 	  System.out.println("Non Abstact method");
+	  System.out.println("Test");
   }
 }
