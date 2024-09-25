f732081e309fdde4a061b9a6b40ae2cf1e66dc16
hp <hp@LAPTOP-TSBJIJT7>
2024-09-25 21:33:13 +0530

    First new commit

---
+++ My_Maven/src/main/java/aggregation/SampleClass1.java
@@ -0,0 +1,12 @@
+package aggregation;
+
+public class SampleClass1 {
+	public void sum() {
+		
+		int a = 20;
+		int b =40;
+		System.out.println("your answer is:" +(a+b ));
+	}
+
+	
+}
---
+++ My_Maven/src/main/java/aggregation/SampleClass2.java
@@ -0,0 +1,20 @@
+package aggregation;
+
+public class SampleClass2 {
+	public void sub() {
+		
+		int a = 50;
+		int b = 30;
+		System.out.println("your anser is :" +(a-b));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleClass1 obj =new SampleClass1();
+		obj.sum();
+		SampleClass2 obj1 =new SampleClass2();
+		obj1.sub();
+
+	}
+
+}
